bbe069db0682fda9a2e1da2e3853e809258fe52e
James Kane <jkane@paradowski.com>
2020-08-21 17:46:15 -0500

    Updating fragment shader

--- src/renderers/WebGLRenderTargetCube.js
+++ src/renderers/WebGLRenderTargetCube.js
@@ -71,11 +71,7 @@ WebGLRenderTargetCube.prototype.fromEquirectangularTexture = function ( renderer
 
 			"	vec3 direction = normalize( vWorldDirection );",
 
-			"	vec2 sampleUV;",
-
-			"	sampleUV.y = asin( clamp( direction.y, - 1.0, 1.0 ) ) * RECIPROCAL_PI + 0.5;",
-
-			"	sampleUV.x = atan( direction.z, direction.x ) * RECIPROCAL_PI2 + 0.5;",
+			"	vec2 sampleUV = equirectUv( direction );",
 
 			"	gl_FragColor = texture2D( tEquirect, sampleUV );",
 
